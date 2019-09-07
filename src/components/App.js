749ab6432b202bd1935bb496f0cf857177a6cc2a
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-09-08 06:42:45 +0900

    イベント一覧に作成したイベントを表示させよう

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useReducer, useState } from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Event from './Event'
 import reducer from '../reducers'
 
 const App = () => {
@@ -44,6 +45,7 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
+          { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />)) }
         </tbody>
       </table>
     </div>
