b3fc7ef33f254dff3af17c26c130aba8eaf564e0
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-09-08 08:54:48 +0900

    操作ログを保存しよう

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,9 +1,12 @@
 import React, { useState, useContext } from 'react'
 import {
   CREATE_EVENT,
-  DELETE_ALL_EVENTS
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_OPERATION_LOGS
 } from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext)
@@ -16,6 +19,11 @@ const EventForm = () => {
       title,
       body,
     })
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601()
+    })
     setTitle('')
     setBody('')
   }
@@ -23,7 +31,14 @@ const EventForm = () => {
   const deleteAllEvents = e => {
     e.preventDefault()
     const result = window.confirm('全てのイベントを本当に削除してもよいですか？')
-    if (result) dispatch({ type: DELETE_ALL_EVENTS })
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENTS })
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601()
+      })
+    }
   }
 
   const unCreatable = title === '' || body === ''
